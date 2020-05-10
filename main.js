6e3e7f2629e7389132738316863212108b2b3393
guyroberts21 <guyroberts21@outlook.com>
2020-05-10 19:20:06 +0100

    Added favicons and basic game working

--- main.js
+++ main.js
@@ -11,26 +11,64 @@ const gameControl = (() => {
         }
     }
 
-    function addMark(e) {
-        e.target.classList.add('slide-in');
-        player1Container.classList.toggle('current-player');
-        player2Container.classList.toggle('current-player');
-        
-        let idx = parseInt(e.target.dataset.key);
-        if (player1Turn) {
-            gameBoard.board[idx] = 'x'; 
-        } else {
-            gameBoard.board[idx] = 'o';
+    function checkWin() {
+        let combos = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        for (let combo of combos) {
+            let num1 = gameBoard.board[combo[0]];
+            let num2 = gameBoard.board[combo[1]];
+            let num3 = gameBoard.board[combo[2]];  
+            if ((num1 === num2 && num1 === num3 && num1 !== null) && num1 !== '') {
+                return true;
+            }
         }
-        player1Turn = !player1Turn;
-        render();
+    }
+
+    function addMark(e) {
+        let idx = parseInt(e.target.dataset.key);
+        
+        // Check if the board square is already taken or not
+        if (gameBoard.board[idx] == '') {
+            if (player1Turn) {
+                gameBoard.board[idx] = 'x'; 
+            } else {
+                gameBoard.board[idx] = 'o';
+            }
+ 
+            // Links to css animation
+            e.target.classList.add('slide-in');
+
+            // Add highlight to current player
+            player1Container.classList.toggle('current-player');
+            player2Container.classList.toggle('current-player');
+
+            // Alternate the player turns
+            player1Turn = !player1Turn;
+            
+            // Re-render every time new mark is added
+            render();
+
+            // Check win
+            if (checkWin()) {
+                alert(`Player ${gameBoard.board[idx] === 'x' ? 1 : 2} wins!`);
+            }
+        } else {
+            return;
+        }
     }
 
     // Allow player to add mark
     const boardSquares = document.querySelectorAll('.board-square');
-    boardSquares.forEach(square => {
-        square.addEventListener('click', addMark);
-    });
+    boardSquares.forEach(square => square.addEventListener('click', addMark));
 
     return {
         render: render,
