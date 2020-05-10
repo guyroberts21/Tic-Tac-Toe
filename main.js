c25dbce25510fc775f1e53697f3f3ae8f5616f0c
Guy Roberts <guyroberts21@outlook.com>
2020-05-10 14:59:19 +0100

    More Styling

--- main.js
+++ main.js
@@ -1,48 +1,50 @@
-const player1Container = document.getElementById('player1')
-const player2Container = document.getElementById('player2')
-const boardSquares = document.querySelectorAll('.board-square');
+const gameControl = (() => {
+    const player1Container = document.getElementById('player1')
+    const player2Container = document.getElementById('player2')
 
-const displayController = (() => {
     let player1Turn = true;
 
-    if (player1Turn) {
-        player1Container.backgroundColor = '#3ff2b6';
+    function render() {
+        // Render the data from the board array
+        for (let i = 0; i < gameBoard.board.length; i++) {
+            boardSquares[i].textContent = gameBoard.board[i];
+        }
     }
 
+    function addMark(e) {
+        e.target.classList.add('slide-in');
+        player1Container.classList.toggle('current-player');
+        player2Container.classList.toggle('current-player');
+        
+        let idx = parseInt(e.target.dataset.key);
+        if (player1Turn) {
+            gameBoard.board[idx] = 'x'; 
+        } else {
+            gameBoard.board[idx] = 'o';
+        }
+        player1Turn = !player1Turn;
+        render();
+    }
+
+    // Allow player to add mark
+    const boardSquares = document.querySelectorAll('.board-square');
+    boardSquares.forEach(square => {
+        square.addEventListener('click', addMark);
+    });
+
     return {
-        player1Turn,
+        render: render,
     }
 })();
 
 const gameBoard = (() => { 
+    'use strict';
     let board = ['', '', '', '', '', '', '', '', ''];
 
     return {
-        board,
-    }
-
+        board: board,
+    };
 })();
 
-const createPlayer = (() => ({
-    score: 0,
-}));
-
-function render() {
-    // Render the data from the board array
-    for (let i = 0; i < gameBoard.board.length; i++) {
-        boardSquares[i].textContent = gameBoard.board[i];
-    }
-}
-
-function addMark(e) {
-    console.log(e.target.dataset.key);
-    let idx = parseInt(e.target.dataset.key);
-    gameBoard.board[idx] = 'x';
-    render();
-}
-
-const player1 = createPlayer();
-const player2 = createPlayer();
-
-boardSquares.forEach(square => square.addEventListener('click', addMark));
-render();
+// Start the game
+gameControl.render();
